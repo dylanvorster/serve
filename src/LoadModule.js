33987aed93d14b9d6ce4b13afd04a70bf3211ca6
Niko Lubbe <niko@eezipay.com>
2015-05-19 18:24:59 +0200

    remove dependency on lodash for LoadModule

--- src/LoadModule.js
+++ src/LoadModule.js
@@ -1,5 +1,4 @@
 var loadScript = require("load-script");
-var _merge = require("lodash/object/merge");
 
 /**
  * Loads Javascript modules into the browser
@@ -14,7 +13,8 @@ window.loadModule = function(file,cb){
 	};
 	if(typeof file === 'object'){
 		f = file.file;
-		params = _merge(file.params,params);
+		params = file.params || {};
+		params.CACHE_ID = window.CACHE_ID;
 	}
 	var final = f+"?";
 	for(var i in params){
